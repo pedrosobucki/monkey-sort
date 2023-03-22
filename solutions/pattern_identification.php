f00a1d51dbec866444295f10dd53f341743e8012
pedrosobucki <pedrosobucki@gmail.com>
2023-03-21 21:42:52 -0300

    commented patter_identification algorithm steps for better undestanding

--- solutions/pattern_identification.php
+++ solutions/pattern_identification.php
@@ -5,9 +5,10 @@ require_once "./common/helper_functions.php";
 
 function sortData(int $rounds, array &$monkeys)
 {
+  // variable for storing monkey hierarchy (most to least coconuts) every round iteration
   $roundHierarchyList = [];
 
-  // printResult($rounds, $monkeys);
+  // stores only even/odd count for each monkey
   $monkeys = countEvensAndOdds($monkeys);
 
   // iterates over all rounds
@@ -23,12 +24,19 @@ function sortData(int $rounds, array &$monkeys)
 
     }
 
+    // stores monkey hierarchy for current round iteration 
     $roundHierarchy = sortMonkeysWithMostCoconuts($monkeys);
+
+    // returns index of pattern start if current round hierarchy has already occured
     $patternStart = array_search($roundHierarchy, $roundHierarchyList);
 
+    // checks if a pattern was found
     if ($patternStart !== false) {
+
+      // pattern ending index is equal to previous round
       $patternEnd = $i-1;
       
+      // finds hierarchy corresponding to desired rounds 
       $finalHierarchy = retrieveFinalMonkeyHierarchy($rounds, $patternStart, $patternEnd, $roundHierarchyList);
 
       printMonkeyHierarchy($finalHierarchy);
@@ -42,6 +50,7 @@ function sortData(int $rounds, array &$monkeys)
 
 function sortMonkeysWithMostCoconuts(array $monkeys): array
 {
+  // stores coconut count for monkeys with more than 0 coconuts total
   for ($i = 0; $i < count($monkeys); $i++) {
     $totalCoconuts = $monkeys[$i]->totalCoconuts();
 
@@ -50,18 +59,25 @@ function sortMonkeysWithMostCoconuts(array $monkeys): array
     }
   }
   
+  // sorts monkeys from most to least coconuts
   arsort($monkeysScore, SORT_NUMERIC);
 
+  // returns list with ordered monkeys
   return array_keys($monkeysScore);
 }
 
 function retrieveFinalMonkeyHierarchy(int $rounds, int $patternStart, int $patternEnd, array &$hierarchyList): array
 {
+  // excludes rounds before pattern start
   $remainingRounds = $rounds - ($patternStart - 1);
+
+  // gets mod between remaining rounds and patter size
   $mod = $remainingRounds % ($patternEnd - $patternStart);
 
+  // finds round with desired hierarchy
   $targetHierarchy = $mod + $patternStart;
 
+  // retrieves hierarchy corresponding to desired rounds 
   $finalHierarchy = $hierarchyList[$targetHierarchy];
 
   return $finalHierarchy;

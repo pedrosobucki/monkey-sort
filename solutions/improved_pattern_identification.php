407cef2e77b986f8bca2247e9377a11671f5de20
pedrosobucki <pedrosobucki@gmail.com>
2023-03-26 17:54:24 -0300

    commented improved pattern identification algorithm for better readability

--- solutions/improved_pattern_identification.php
+++ solutions/improved_pattern_identification.php
@@ -27,33 +27,51 @@ function sortData(int $rounds, array &$monkeys)
 
     }
 
+    // creates array containing only mokneys with coconuts != 0 and stores array length
     $monkeysWithCoconuts = mapCoconutCount($monkeys);
     $currentRoundMonkeyCount = count($monkeysWithCoconuts);
 
+    // searches for monkey with most coconuts in current round
     $roundWinner = array_search(max($monkeysWithCoconuts),$monkeysWithCoconuts);
 
+    // checks if the number of monkeys with more than 0 coconuts has decresed from past round and, if so
     if ($currentRoundMonkeyCount < $lastRoundMonkeyCount) {
 
+      // replaces 'last round count' for 'current count' for next loop iteration
       $lastRoundMonkeyCount = $currentRoundMonkeyCount;
+
+      // increments controll variable for array position in which the pattern starts (will start when the quantity of 
+      // monkeys with more than 0 coconuts does not chage from 1 round to the next )
       $patternStart++;
+
+      // sotres current round winner in array
       $roundWinners[] = $roundWinner;
 
       continue;
     }
 
+    // if last and current round quantity of monkeys with more than 1 coconut are equal
+    // AND this is the round right after the pattern has started:
     if ($i === $patternStart + 1) {
+      // leaves only 'pattern start' round winner monkeys in pattern list
       $roundWinners = [$roundWinners[$patternStart]];
     }
 
-    $roundWinner = array_search(max($monkeysWithCoconuts),$monkeysWithCoconuts);
+    // checks if current round winner has already appeared in pattern (found the end of the pattern)
     $patternExists = array_search($roundWinner, $roundWinners);
     
+    // if pattern exists:
     if ($patternExists !== false) {
+
+      // retrieves winner monkey
       $winnerMonkey = retrieveWinnerMonkey($rounds, $patternStart, $roundWinners);
+
+      // prints information to screen
       echo "Winner: Monkey {$winnerMonkey}\n";
       die;
     }
 
+    // stores round winner in array if pattern was not found
     $roundWinners[] = $roundWinner;
   }
 
@@ -63,21 +81,21 @@ function retrieveWinnerMonkey(int $rounds, int $patternStart, array &$roundWinne
 {
   echo "pattern start position: {$patternStart}\n";
 
+  // calculates pattern size
   $patternSize = count($roundWinners);
   echo "pattern size: {$patternSize}\n";
 
-  // excludes rounds before pattern start
+  // excludes total rounds before pattern start
   $remainingRounds = $rounds - ($patternStart);
-
   echo "remaining rounds: {$remainingRounds}\n";
 
   // gets mod between remaining rounds and patter size
   $mod = ($remainingRounds - 1) % $patternSize;
   echo "mod: {$mod}\n";
 
-  // retrieves hierarchy corresponding to desired rounds 
+  // winner monkey from pattern position 
   $finalWinner = $roundWinners[$mod];
-
   echo "\n";
+  
   return $finalWinner;
 }
